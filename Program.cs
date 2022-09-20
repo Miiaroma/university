af343c2804d0b80367a93ae6f4b8675a3f17990d
Miiaroma <miia.romanainen@gmail.com>
2022-09-20 19:19:31 +0300

    Program file modified.

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 using university;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -7,22 +8,32 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-builder.Services.AddTransient(_ => new Database(builder.Configuration.GetConnectionString("DefaultConnection")));
 
+builder.Services.AddTransient(_ => new Database(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+/* if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
-}
+} */
+    app.UseSwagger();
+    app.UseSwaggerUI();
 
 app.UseHttpsRedirection();
 
 app.UseAuthorization();
 
 app.MapControllers();
+
+//example about MiddleWare function
+app.Use(async (contex, next)=>
+{
+    Console.WriteLine("Middleware excuted");
+    await next();
+}
+);
 
 app.Run();
