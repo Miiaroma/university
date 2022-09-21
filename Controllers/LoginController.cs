569a50f833353f7e4a18d8a47653d38cab0321da
Miiaroma <miia.romanainen@gmail.com>
2022-09-21 09:57:27 +0300

    Login added.

---
+++ Controllers/LoginController.cs
@@ -0,0 +1,42 @@
+using Microsoft.AspNetCore.Mvc;
+using university.Models;
+
+namespace university.Controllers
+{
+    [Route("api/[controller]")]
+    public class LoginController : ControllerBase
+    {
+        public LoginController(Database db)
+        {
+            Db = db;
+        }
+
+
+        // POST api/login
+        [HttpPost]
+        public async Task<IActionResult> Post([FromBody]User body)
+        {
+            Console.WriteLine(body.username);
+            Console.WriteLine(body.password);
+            await Db.Connection.OpenAsync();
+            var query = new Login(Db);
+            var passwordFromTheDtabase = await query.GetPassword(body.username);
+   
+            if (passwordFromTheDtabase is null || ! BCrypt.Net.BCrypt.Verify(body.password, passwordFromTheDtabase))
+            {
+                // authentication failed
+                return new OkObjectResult(false);
+            }
+            else
+            {
+                // authentication successful
+                return new OkObjectResult(true);
+            }
+            
+        }
+
+    
+
+        public Database Db { get; }
+    }
+}
